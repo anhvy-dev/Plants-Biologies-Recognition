358b3f2964faf7fda8a5256fc7377a0f9705fde4
anhvydabeginner <anhvse181832@fpt.edu.vn>
2025-06-02 22:45:16 +0700

    create landing page

---
+++ Plants-Biologies-Recognition/src/landing-page/LandingPage.jsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import AppTheme from "../shared-theme/AppTheme.jsx";
+import AppAppBar from "./components/AppAppBar.jsx";
+import Hero from "./components/Hero.jsx";
+import LogoCollection from "./components/LogoCollection.jsx";
+import Highlights from "./components/Highlights.jsx";
+import Pricing from "./components/Pricing.jsx";
+import Features from "./components/Features.jsx";
+import Testimonials from "./components/Testimonials.jsx";
+import FAQ from "./components/FAQ.jsx";
+import Footer from "./components/Footer.jsx";
+
+export default function MarketingPage(props) {
+  return (
+    <AppTheme {...props}>
+      <CssBaseline enableColorScheme />
+      <AppAppBar />
+      <Hero />
+      <div>
+        <LogoCollection />
+        <Features />
+        <Divider />
+        <Testimonials />
+        <Divider />
+        <Highlights />
+        <Divider />
+        <Pricing />
+        <Divider />
+        <FAQ />
+        <Divider />
+        <Footer />
+      </div>
+    </AppTheme>
+  );
+}
--- Plants-Biologies-Recognition/src/main.jsx
+++ Plants-Biologies-Recognition/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import LandingPage from "./landing-page/LandingPage.jsx";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <LandingPage />
+  </StrictMode>
+);
