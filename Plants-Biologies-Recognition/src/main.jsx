70898244a2e5013720c10114a8638c55bfc1ed37
anhvydabeginner <anhvse181832@fpt.edu.vn>
2025-06-07 16:45:28 +0700

    add dashboard

--- Plants-Biologies-Recognition/src/main.jsx
+++ Plants-Biologies-Recognition/src/main.jsx
@@ -5,16 +5,22 @@ import App from "./App.jsx";
 import LandingPage from "./landing-page/LandingPage.jsx";
 import SignUp from "./sign-up/SignUp.jsx";
 import SignIn from "./sign-in/SignIn.jsx";
+import Dashboard from "./dashboard/Dashboard.jsx";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-      </Routes>
-    </Router>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
+    </LocalizationProvider>
   </StrictMode>
 );
