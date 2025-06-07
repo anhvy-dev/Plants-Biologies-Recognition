9e732c6ae92263665a864f7f8108ce7f0924771b
anhvydabeginner <anhvse181832@fpt.edu.vn>
2025-06-07 16:10:06 +0700

    add signin and signup

--- Plants-Biologies-Recognition/src/main.jsx
+++ Plants-Biologies-Recognition/src/main.jsx
@@ -3,10 +3,18 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import LandingPage from "./landing-page/LandingPage.jsx";
+import SignUp from "./sign-up/SignUp.jsx";
+import SignIn from "./sign-in/SignIn.jsx";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <LandingPage />
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </Router>
   </StrictMode>
 );
