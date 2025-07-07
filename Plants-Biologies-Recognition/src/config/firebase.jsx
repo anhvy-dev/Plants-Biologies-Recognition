4a7d47801c04bd2157430379c48d4dac6e829c08
anhvydabeginner <anhvse181832@fpt.edu.vn>
2025-07-07 17:31:11 +0700

    add .env

--- Plants-Biologies-Recognition/src/config/firebase.jsx
+++ Plants-Biologies-Recognition/src/config/firebase.jsx
@@ -1,19 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDJexbI-idgxZJBtIbCZ43o9oZipfjfvcg",
-  authDomain: "plants-biologies-recognition.firebaseapp.com",
-  projectId: "plants-biologies-recognition",
-  storageBucket: "plants-biologies-recognition.firebasestorage.app",
-  messagingSenderId: "100498789921",
-  appId: "1:100498789921:web:6369d99657c80e46707560",
-  measurementId: "G-S7B83X9DFG",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
