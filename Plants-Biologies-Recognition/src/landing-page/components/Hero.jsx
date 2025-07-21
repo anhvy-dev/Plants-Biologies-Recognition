a18f07d0ae30d34542db346220349922cccf05e3
anhvydabeginner <anhvse181832@fpt.edu.vn>
2025-07-21 23:34:39 +0700

    change landingpage image

--- Plants-Biologies-Recognition/src/landing-page/components/Hero.jsx
+++ Plants-Biologies-Recognition/src/landing-page/components/Hero.jsx
@@ -22,7 +22,7 @@ const StyledBox = styled("div")(({ theme }) => ({
   border: "1px solid",
   borderColor: (theme.vars || theme).palette.grey[200],
   boxShadow: "0 0 12px 8px hsla(220, 25%, 80%, 0.2)",
-  backgroundImage: `url(/src/assets/weblight.png)`,
+  backgroundImage: `url(https://res.cloudinary.com/dllmbutza/image/upload/v1753115020/weblight_wkvxyt.png)`,
   backgroundSize: "cover",
   backgroundPosition: "center",
   [theme.breakpoints.up("sm")]: {
@@ -31,7 +31,7 @@ const StyledBox = styled("div")(({ theme }) => ({
   },
   ...theme.applyStyles("dark", {
     boxShadow: "0 0 24px 12px hsla(210, 100%, 25%, 0.2)",
-    backgroundImage: `url(/src/assets/webdark.png)`,
+    backgroundImage: `url(https://res.cloudinary.com/dllmbutza/image/upload/v1753115020/webdark_g7djuj.png)`,
     outlineColor: "hsla(220, 20%, 42%, 0.1)",
     borderColor: (theme.vars || theme).palette.grey[700],
   }),
